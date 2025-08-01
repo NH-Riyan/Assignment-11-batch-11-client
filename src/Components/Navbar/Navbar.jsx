93ab00713c1e946b6028fad91b84ad8a63b39ddd
NH-Riyan <nazmulhasanriyan@gmail.com>
2025-08-01 12:05:21 +0600

    added nav,footer

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Navbar = () => {
+  const link = <>
+    <li>Home</li>
+    <li>Courses</li>
+    <li>Add Courses</li>
+    <li>My Courses</li>
+  </>
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content gap-5 bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            {link}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl italic">Course Era</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 gap-5">
+          {link}
+        </ul>
+      </div>
+      <div className='flex gap-4 navbar-end'>
+      <Link to='/auth/login' className='btn btn-primary px-7'>Login</Link>
+      <Link to='/auth/register' className='btn btn-primary px-7'>SignUp</Link>
+    </div>
+      </div >
+    );
+};
+
+export default Navbar;
